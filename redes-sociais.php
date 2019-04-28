b76c76040542aa6730971cda335c86e22ea2f711
1937600 <37963608+1937600@users.noreply.github.com>
2019-04-28 20:32:49 -0300

    Add files via upload
    atualiza página de currículo e deixa o visual mais simples

---
+++ redes-sociais.php
@@ -0,0 +1,159 @@
+<!doctype html>
+
+<html lang="pt-br">
+
+<head>
+
+	<!-- Título para a guia da página -->
+	<title>Redes Sociais - Saulo da Silva</title>
+
+	<!-- Autoria da página -->
+	<meta name="author" content="Saulo de Freitas Martins da Silva">
+
+	<!-- Descrição do site -->
+	<meta name="description" content="Redes sociais do Saulo da Silva">
+
+	<!-- Palavras-chave -->
+	<meta name="keywords" content="Engenheiro de Software, Desenvolvedor, Programador, Criador de Sites, Designer, Programa&ccedil;&atilde;o, Software, Design, Site">
+
+	<!-- Define a codificação de caracteres -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+	<!-- Responsividade: formatação da página em diversos dispositivos -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Recarrega a página a cada 60 segundos(DESATIVADO PORQUE CAUSA -->
+	<!-- FALHAS EM SISTEMAS DE ACESSIBILIDADE PARA LEITURA DA PÁGINA)  -->
+	<!-- <meta http-equiv="refresh" content="30">					   -->
+    
+	<!-- ESTILO -->
+    <!-- FAVICON -->
+    <link
+        rel="icon"
+        type="imagem/png"
+        href="img/favicon.png"
+    >
+    <!-- FONTE -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400"
+        rel="stylesheet"
+    >
+    <!-- BIBLIOTECA: BOOTSTRAP -->
+	<link
+		rel="stylesheet"
+		href="css/bootstrap.min.css"
+	>
+    <!-- PRINCIPAL -->
+	<link
+		rel="stylesheet"
+		href="css/main.css"
+	>
+
+    <!-- COMPORTAMENTO -->
+    <!-- 
+    <script
+        type="text/javascript"
+        src="js/index.js"
+    ></script>
+    -->
+    
+</head>
+
+<body>
+    <nav role="navigation" class="navbar navbar-expand-md fixed-top">
+        <div id="logo">
+            <a href="/"><span id="triangulo-peq"></span><span id="s-peq">S</span></a>
+        </div>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarCollapse">
+            <ul class="navbar-nav">
+                <li class="botaoMenu nav-item"><a href="/" class="nav-link">In&iacute;cio</a></li>
+                <li class="botaoMenu nav-item"><a href="curriculo" class="nav-link">Curr&iacute;culo</a></li>
+                <li class="botaoMenu nav-item"><a href="portfolio" class="nav-link">Portf&oacute;lio</a></li>
+                <li class="botaoPagAtual nav-item"><a href="redes-sociais" class="nav-link">Redes Sociais</a></li>
+                <li class="botaoMenu nav-item"><a href="contato" class="nav-link">Contato</a></li>
+            </ul>
+        </div>
+    </nav>
+    <header></header>
+    <main class="container">
+        <div class="card-deck mx-auto">
+                <div class="card text-center">
+            
+                    <div class="card-body">
+                        <img src="/img/redes-sociais/in.png" alt="Logomarca do Linkedin">
+                        <h3 class="card-title">LinkedIn</h3>
+                        <p class="card-text"><a href="https://www.linkedin.com/in/saulodasilva/" target="_blank">saulodasilva</a></p>
+                    </div>
+            
+                </div>
+                <div class="card text-center">
+            
+                    <div class="card-body">
+                        <img src="/img/redes-sociais/fb.png" alt="Logomarca do Facebook">
+                        <h3 class="card-title">Facebook</h3>
+                        <p class="card-text"><a href="https://www.facebook.com/sau.jjj" target="_blank">sau.jjj</a></p>
+                    </div>
+                </div>
+                <div class="card text-center">
+            
+                    <div class="card-body">
+                        <img src="/img/redes-sociais/wa.png" alt="Logomarca do WhatsApp">
+                        <h3 class="card-title">WhatsApp</h3>
+                        <p class="card-text"><a href="tel:+5541984603893" target="_blank">(41) 984603893</a></p>
+                    </div>
+                </div>
+                <div class="card text-center">
+            
+                    <div class="card-body">
+                        <img src="/img/redes-sociais/insta.png" alt="Logomarca do Instagram">
+                        <h3 class="card-title">Instagram</h3>
+                        <p class="card-text"><a href="https://www.instagram.com/dino.saulo/" target="_blank">dino.saulo</a></p>
+                    </div>
+                </div>
+                <div class="card text-center">
+            
+                    <div class="card-body">
+                        <img src="/img/redes-sociais/tt.png" alt="Logomarca do Twitter">
+                        <h3 class="card-title">Twitter</h3>
+                        <p class="card-text"><a href="https://twitter.com/sau_luxo" target="_blank">sau_luxo</a></p>
+                    </div>
+                </div>
+                <div class="card text-center">
+            
+                    <div class="card-body">
+                        <img src="/img/redes-sociais/spotify.png" alt="Logomarca do Spotify">
+                        <h3 class="card-title">Spotify</h3>
+                        <p class="card-text"><a href="https://open.spotify.com/user/saulomart" target="_blank">saulomart</a></p>
+                    </div>
+                </div>
+        </div>
+    </main>
+    <footer role="contentinfo" class="footer">
+        <div class="container">
+            <span>Atualizado pela &uacute;ltima vez 29/01/2019.</span>
+        </div>
+    </footer>
+    <!-- BIBLIOTECA: JQUERY -->
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+    ></script>
+    <!-- BIBLIOTECA: POPPER -->
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"
+    ></script>
+    <!-- BIBLIOTECA: BOOTSTRAP -->
+    <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"
+    ></script>
+</body>
+
+</html>
